b2798be8734343992dddefb64a3c2eee89b9f1ae
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2017-12-18 14:43:49 +0530

    feat: Add Cut width seekbar

--- app/src/main/java/me/shellbell/makepizza/MainActivity.kt
+++ app/src/main/java/me/shellbell/makepizza/MainActivity.kt
@@ -10,15 +10,22 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var pizza: Pizza
     private lateinit var edgeWidthSeekBar: SeekBar
+    private lateinit var cutWidthSeekBar: SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         edgeWidthSeekBar = findViewById(R.id.edge_width_seek_bar)
+        cutWidthSeekBar = findViewById(R.id.cut_width_seek_bar)
         pizza = findViewById(R.id.pizza)
 
-        pizza.post { setupEdgeWidthSeekBar() }
+        pizza.post { setupSeekBars() }
+    }
+
+    private fun setupSeekBars() {
+        setupEdgeWidthSeekBar()
+        setupCutWidthSeekBar()
     }
 
     private fun setupEdgeWidthSeekBar() {
@@ -26,11 +33,29 @@ class MainActivity : AppCompatActivity() {
         val height = pizza.height - pizza.paddingTop - pizza.paddingBottom
 
         edgeWidthSeekBar.max = min(width, height) / 2
-
         edgeWidthSeekBar.progress = pizza.edgeWidth.toInt()
+
         edgeWidthSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, p2: Boolean) {
                 pizza.edgeWidth = progress.toFloat()
+                pizza.invalidate()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+    }
+
+    private fun setupCutWidthSeekBar() {
+        val MAX_CUT_WIDTH = 24F
+
+        cutWidthSeekBar.max = MAX_CUT_WIDTH.toInt()
+        cutWidthSeekBar.progress = pizza.cutWidth.toInt()
+
+        cutWidthSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, p2: Boolean) {
+                pizza.cutWidth = progress.toFloat()
                 pizza.invalidate()
             }
 
--- pizza/src/main/java/me/shellbell/pizza/Pizza.kt
+++ pizza/src/main/java/me/shellbell/pizza/Pizza.kt
@@ -6,7 +6,6 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
-import kotlin.math.max
 import kotlin.math.min
 
 /**
